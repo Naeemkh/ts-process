8ca24e88cbd527c8b1520b9693119aee99f38c10
naeem <khoshnevis.naeem@gmail.com>
2020-07-07 22:53:48 -0500

    created setup.py file.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ts-process
+
 Ground motion time series processing tools
 
 This is a collection of Python3-based software programs that are ground motions time series processing utilities designed to integrate 3D ground motion simulation seismograms from AWP-ODC and Hercules with Broadband Platform time series. The ts-process library also provides codes for calculating ROTD50 so that a common implementation is used to process both 3D simulation seismograms and 1D broadband platform seismograms.
@@ -10,6 +11,7 @@ These codes have been developed as part of earthquake ground motion research per
 * Ricardo Taborda - Universidad EAFIT Medellín Colombia
 * Christine Goulet - University of Southern California
 * Fabio Silva - Southern California Earthquake Center
+* Naeem Khoshnevis - University of Memphis
 
 ## Software support:
 * software @ scec.org 
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ts-process", 
+    version="0.0.1",
+    author="Naeem Khoshnevis, Fabio Silva",
+    author_email="khoshnevis.naeem@gmail.com,fsilva@usc.edu",
+    maintainer="Naeem Khoshnevis, Ricardo Taborda, Christine Goulet, Fabio Silva",
+    maintainer_email = "khoshnevis.naeem@gmail.com,rtaborda@eafit.edu.co,cgoulet@usc.edu,fsilva@usc.edu",
+    description="Ground motion time series processing tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Naeemkh/ts-process",
+    license="BSD 3-Clause",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Computational Seismology",
+        "Programming Language :: Python :: 3 :: only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    python_requires='>=3.7',
+)
