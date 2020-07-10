14f6951f5dae16f90d13affadfe62353b2caff31
naeem <khoshnevis.naeem@gmail.com>
2020-07-09 21:46:40 -0500

    Added reading from hercules format.
    Co-authored-by: Fabio Silva <fsilva@usc.edu>
    Co-authored-by: Ricardo Taborda <ricardotaborda@gmail.com>

--- tsprocess/record.py
+++ tsprocess/record.py
@@ -6,6 +6,8 @@ The core module for the Record class.
 
 from .database import DataBase
 import hashlib
+from .timeseries import  Disp, Vel, Acc, Raw, Unitless
+import numpy as np 
 
 class Record:
     """ Record Class """
@@ -99,3 +101,96 @@ class Record:
             value = Record._from_hercules(filename)
             Record.mydb.set_value(hash_val,value)
             return value
+
+    @staticmethod
+    def _from_hercules(filename,station_obj):
+        times = []
+        acc_h1 = []
+        vel_h1 = []
+        dis_h1 = []
+        acc_h2 = []
+        vel_h2 = []
+        dis_h2 = []
+        acc_ver = []
+        vel_ver = []
+        dis_ver = []
+        dis_header = []
+        vel_header = []
+        acc_header = []
+
+        try:
+            input_fp = open(filename, 'r')
+        
+            for line in input_fp:
+                line = line.strip()
+                # Skip comments
+                if line.startswith("#") or line.startswith("%"):
+                    pieces = line.split()[1:]
+                    # Write header
+                    if len(pieces) >= 10:
+                        dis_header.append("# her header: # %s %s %s %s\n" %
+                                         (pieces[0], pieces[1], pieces[2], pieces[3]))
+                        vel_header.append("# her header: # %s %s %s %s\n" %
+                                         (pieces[0], pieces[4], pieces[5], pieces[6]))
+                        acc_header.append("# her header: # %s %s %s %s\n" %
+                                         (pieces[0], pieces[7], pieces[8], pieces[9]))
+                    else:
+                        dis_header.append("# her header: %s\n" % (line))
+                    continue
+                pieces = line.split()
+                pieces = [float(piece) for piece in pieces]
+                # Write timeseries to files. Please not that Hercules files have
+                # the vertical component positive pointing down so we have to flip it
+                # here to match the BBP format in which vertical component points up
+                times.append(pieces[0])
+                dis_h1.append(pieces[1])
+                dis_h2.append(pieces[2])
+                dis_ver.append(-1 * pieces[3])
+                vel_h1.append(pieces[4])
+                vel_h2.append(pieces[5])
+                vel_ver.append(-1 * pieces[6])
+                acc_h1.append(pieces[7])
+                acc_h2.append(pieces[8])
+                acc_ver.append(-1 * pieces[9])
+        except IOError as e:
+            print(e)
+            sys.exit(-1)
+        finally:
+            # All done
+            input_fp.close()
+        # Convert to NumPy Arrays
+        times = np.array(times)
+        vel_h1 = np.array(vel_h1)
+        vel_h2 = np.array(vel_h2)
+        vel_ver = np.array(vel_ver)
+        acc_h1 = np.array(acc_h1)
+        acc_h2 = np.array(acc_h2)
+        acc_ver = np.array(acc_ver)
+        dis_h1 = np.array(dis_h1)
+        dis_h2 = np.array(dis_h2)
+        dis_ver = np.array(dis_ver)
+
+        dt = times[1] - times[0]
+
+
+        disp_h1 = Disp(dis_h1, dt, times[0])
+        disp_h2 = Disp(dis_h2, dt, times[0])
+        disp_ver = Disp(dis_ver, dt, times[0])
+    
+        vel_h1 = Vel(vel_h1, dt, times[0])
+        vel_h2 = Vel(vel_h2, dt, times[0])
+        vel_ver = Vel(vel_ver, dt, times[0])
+    
+        acc_h1 = Acc(acc_h1, dt, times[0])
+        acc_h2 = Acc(acc_h2, dt, times[0])
+        acc_ver = Acc(acc_ver, dt, times[0])
+    
+        # self.delta_t = times[1] - times[0]
+        # Group headers
+        # headers = [dis_header, vel_header, acc_header]
+
+        return Record(times, disp_h1, disp_h2, disp_ver,
+                    vel_h1, vel_h2, vel_ver,
+                    acc_h1, acc_h2, acc_ver,
+                    station_obj)
+
